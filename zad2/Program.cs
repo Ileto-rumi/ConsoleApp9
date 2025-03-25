b4239b512ce461b5f6d56740f24eb136d316fb71
MEGA <MEGA@gmail.com>
2025-03-25 18:02:48 +0200

    zad 2

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> dumi = new Dictionary<string, int>();
+            Dictionary<string, int> chestota = new Dictionary<string, int>();
             string vhod = Console.ReadLine();
+            string[] words = vhod.Split(new[] { ' ', '.', ',', ';', '!', '?', '-', ':', '(', ')', '[', ']', '{', '}', '"' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                string dumi = word.ToLower(); // Преобразуваме всяка дума в малки букви за да избегнем дублиране на същите думи с различни регистри
+
+                if (chestota.ContainsKey(dumi))
+                {
+                    chestota[dumi]++;
+                }
+                else
+                {
+                    chestota.Add(dumi, 1);
+                }
+            }
+            Console.WriteLine("Chestota:");
+            foreach (var duma in chestota)
+            {
+                Console.WriteLine($"Дума: '{duma.Key}', Честота: {duma.Value}");
+            }
         }
     }
 }
